22a048e3909ed1488b3de72fee15f0205bd0f9d0
Aznair25 <aznairmanzoor1@gmail.com>
2025-03-12 11:01:47 +0500

    improved snippet

--- abc/research.py
+++ abc/research.py
@@ -1,7 +1,9 @@
+from tabulate import tabulate
 import urllib,urllib.request
 import xmltodict
 import json
 count=0
+table=[]
 user_input= input("Enter the topic name: ")
 l=user_input.split(" ")
 user_input="+".join(l)
@@ -17,19 +19,25 @@ with open("research.json" ,"w",encoding="utf-8") as json_file:
     json_data=json.dump(dic_data, json_file, indent=4)
 with open("research.json","r") as json_file:
     data=json.load(json_file)
+    in_table={}
     while count<len(data["feed"]["entry"]):
         title=data["feed"]["entry"][count]["title"]
-        print(f"Paper # {count+1}")
-        print(f"Title of the Paper: {title}")
+        in_table["Title"]=title
         author=data["feed"]["entry"][count]["author"]
         if isinstance(author,list):
-            for index in range(len(author)): 
-                print(f"Author : {author[index]["name"]}")
+            for index in range(len(author)):
+                name=author[index]["name"]
+                if not "Author Name" in in_table.keys():
+                    in_table["Author Name"]=[]
+                    in_table["Author Name"].append(name)
+                else:
+                    in_table["Author Name"].append(name)
         else:
-            print(f"Author: {author["name"]}")
+            name=author["name"]
+            in_table["Author Name"]=name
         summary=data["feed"]["entry"][count]["summary"]
-        print(f"Summary: {summary}")
+        in_table["Summary"]=summary
+        table.extend([in_table])
         count+=1
-       
-
-# declare a variable to iterate using index number [0] 
+        in_table={}
+print(tabulate(table, headers="keys",tablefmt="fancy_grid",showindex="always"))
